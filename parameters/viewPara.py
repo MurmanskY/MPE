ba821858f280ea081a371f65b5ccf2b8e935e9f1
MurmanskY <642015814@qq.com>
2024-08-05 18:41:18 +0800

    feat(modelStructure): update pth  dic key

--- parameters/viewPara.py
+++ parameters/viewPara.py
@@ -1,5 +1,15 @@
 import torch
 
+
+resnet18InitParaPath = './init/resnet18-f37072fd.pth'
+resnet50InitParaPath = './init/resnet50-11ad3fa6.pth'
+resnet101InitParaPath = './init/resnet101-cd907fc2.pth'
+vgg11InitParaPath = './init/vgg11-8a719046.pth'
+vgg13InitParaPath = './init/vgg13-19584684.pth'
+vgg16InitParaPath = './init/vgg16-397923af.pth'
+vgg19InitParaPath = './init/vgg19-dcbb9e9d.pth'
+
+
 def showParaStructure(paraPath):
     """
     根据pth文件，显示参数的类型
@@ -33,7 +43,6 @@ def showParaValue(paraPath):
 
 if __name__ == "__main__":
     '''for test'''
-    resnet50InitParaPath = "./init/resnet50-11ad3fa6.pth"
-    vgg11InitParaPath = './init/vgg11-8a719046.pth'
-    showParaStructure(resnet50InitParaPath)
+
+    showParaStructure(resnet101InitParaPath)
     # showParaValue(paraPath)
