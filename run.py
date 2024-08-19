b9268da1b454e126340340f41a4da0084c698ce8
MurmanskY <642015814@qq.com>
2024-08-19 21:56:11 +0800

    feat(syn): 20240819 21:56

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 # TODO: add entry python code
+import torch
+import numpy as np
+import matplotlib
+from bitstring import BitArray
+
+if __name__ == "__main__":
+    """for entry of this python program"""
+    
