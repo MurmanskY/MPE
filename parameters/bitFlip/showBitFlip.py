8bbdd92c488fd3edc603355459911e239b42eabb
MurmanskY <642015814@qq.com>
2024-09-02 20:25:15 +0800

    feat(syn): 20240902 20:25

--- parameters/bitFlip/showBitFlip.py
+++ parameters/bitFlip/showBitFlip.py
@@ -89,7 +89,7 @@ def showBitFlip(initParaPath, retrainParaPath, bitStartIdx, bitEndIdx, outputFil
 
     for key in initPara.keys():  # 遍历所有键值
 
-        if len(initPara[key].data.shape) <= 1:
+        if len(initPara[key].data.shape) < 1:
             continue  # 只比较参数二维及以上维度可能嵌入的层
 
         initLayerTensor = initPara[key].data.flatten()  # 将多维向量平铺
@@ -104,30 +104,18 @@ def showBitFlip(initParaPath, retrainParaPath, bitStartIdx, bitEndIdx, outputFil
         for idx in range(paraNum):
             initLayerEleStr = BitArray(int=initLayerTensor[idx].view(torch.int32), length=32).bin[bitStartIdx: bitEndIdx]
             retrainLayerEleStr = BitArray(int=retrainLayerTensor[idx].view(torch.int32), length=32).bin[bitStartIdx: bitEndIdx]
-            print(initLayerEleStr, retrainLayerEleStr)
+            # print(initLayerEleStr, retrainLayerEleStr)
             bitFlipNum += getBitFlipNum(initLayerEleStr, retrainLayerEleStr)
 
         data[key] = bitFlipNum / (paraNum * (bitEndIdx - bitStartIdx))
 
-        print(key, data[key], paraNum)
+        print(key, paraNum, data[key])
 
     df = pd.DataFrame([data])
 
     df.to_csv(outputFile, index=False)
     return
 
-
-def func(pth1, pth2, *layers):
-    para1 = torch.load(pth1)
-    para2 = torch.load(pth2)
-    for layer in layers:
-        if len(para1[layer].data.shape) <= 1:
-            continue  # 只比较参数二维及以上维度可能嵌入的层
-
-        print(format(para1[layer].data[0][0][0][0].view(torch.int32), '032b')[0:4])
-        print(format(para2[layer].data[0][0][0][0].view(torch.int32), '032b')[0:4])
-
-    return
 
 def layerBitFLip(initParaPath, flipParaPath, bit_n, *layers):
     """
@@ -140,34 +128,63 @@ def layerBitFLip(initParaPath, flipParaPath, bit_n, *layers):
     para = torch.load(initParaPath)
 
     for layer in layers:  # layers数组中的所有layer
-        if len(para[layer].data.shape) <= 1:
+        if len(para[layer].data.shape) < 1:
             continue  # 单值除去
         # print(layer, type(layer))
         layerTensor = para[layer].data
         # print(layerTensor.shape)
         layerTensor_initView = layerTensor.view(torch.int32)
-        print(format(layerTensor_initView[0][0][0][0], '032b'), layerTensor[0][0][0][0])
+        # print(format(layerTensor_initView[0][0][0][0], '032b'), layerTensor[0][0][0][0])
         layerTensor_embedded_int = layerTensor_initView ^ bit_n
         layerTensor_embedded = layerTensor_embedded_int.view(torch.float32)
-        print(format(layerTensor_embedded_int[0][0][0][0], '032b'), layerTensor_embedded[0][0][0][0])
-
-
+        # print(format(layerTensor_embedded_int[0][0][0][0], '032b'), layerTensor_embedded[0][0][0][0])
 
         para[layer].data = layerTensor_embedded
-
-        temp = para[layer].data = layerTensor.view(torch.int32)
-        print(format(temp[0][0][0][0], '032b'))
 
     torch.save(para, flipParaPath)
     return
 
 
+def func(pth1, pth2, *layers):
+    para1 = torch.load(pth1)
+    para2 = torch.load(pth2)
+    for layer in layers:
+        if len(para1[layer].data.shape) <= 1:
+            continue  # 只比较参数二维及以上维度可能嵌入的层
+        print(layer, para1[layer].data.shape)
+        para1Tensor = para1[layer].data.flatten()
+        para2Tensor = para2[layer].data.flatten()
+        print(format(para1Tensor[0].view(torch.int32), '032b')[9:32])
+        print(format(para2Tensor[0].view(torch.int32), '032b')[9:32], "\n")
+
+    return
+
+
 if __name__ == "__main__":
     '''resnet50 bit flip'''
-    layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_1.pth", bit_16, *getPthKeys(resnet50InitParaPath))
-    # showBitFlip(resnet50InitParaPath, "./resnet50/bitFlip/frac_1.pth", 0, 4, "./result/temp.csv")
-    # func(resnet50InitParaPath, "./resnet50/bitFlip/frac_1.pth", *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_1.pth", bit_1, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_4.pth", bit_4, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_8.pth", bit_8, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_12.pth", bit_12, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_16.pth", bit_16, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_20.pth", bit_20, *getPthKeys(resnet50InitParaPath))
+    # layerBitFLip(resnet50InitParaPath, "./resnet50/bitFlip/frac_23.pth", bit_23, *getPthKeys(resnet50InitParaPath))
     '''resnet50_2_CFIAR100'''
+    showBitFlip("./resnet50/bitFlip/frac_1.pth", "./resnet50/2CIFAR100/frac_1_ep_5.pth", 31, 32,
+                "./resnet50/2CIFAR100/result/frac_1_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_4.pth", "./resnet50/2CIFAR100/frac_4_ep_5.pth", 28, 32,
+                "./resnet50/2CIFAR100/result/frac_4_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_8.pth", "./resnet50/2CIFAR100/frac_8_ep_5.pth", 24, 32,
+                "./resnet50/2CIFAR100/result/frac_8_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_12.pth", "./resnet50/2CIFAR100/frac_12_ep_5.pth", 20, 32,
+                "./resnet50/2CIFAR100/result/frac_12_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_16.pth", "./resnet50/2CIFAR100/frac_16_ep_5.pth", 16, 32,
+                "./resnet50/2CIFAR100/result/frac_16_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_20.pth", "./resnet50/2CIFAR100/frac_20_ep_5.pth", 12, 32,
+                "./resnet50/2CIFAR100/result/frac_20_ep_5.csv")
+    showBitFlip("./resnet50/bitFlip/frac_23.pth", "./resnet50/2CIFAR100/frac_23_ep_10.pth", 9, 32,
+                "./resnet50/2CIFAR100/result/frac_23_ep_10.csv")
 
+    # func(resnet50InitParaPath, "./resnet50/bitFlip/frac_23.pth", *getPthKeys(resnet50InitParaPath))
 
 
