40220024da93daaaab00e920e2ed7da85a7d786c
MurmanskY <642015814@qq.com>
2024-09-02 09:45:33 +0800

    feat(syn): 20240902 09:45

--- parameters/bitFlip/showBitFlip.py
+++ parameters/bitFlip/showBitFlip.py
@@ -5,15 +5,48 @@ from bitstring import BitArray
 
 device = torch.device("mps")
 
-resnet18InitParaPath = './init/resnet18-f37072fd.pth'
-resnet50InitParaPath = './init/resnet50-11ad3fa6.pth'
-resnet101InitParaPath = './init/resnet101-cd907fc2.pth'
-vgg11InitParaPath = './init/vgg11-8a719046.pth'
-vgg13InitParaPath = './init/vgg13-19584684.pth'
-vgg16InitParaPath = './init/vgg16-397923af.pth'
-vgg19InitParaPath = './init/vgg19-dcbb9e9d.pth'
+'''原始pth文件'''
+resnet18InitParaPath = '../init/resnet18-f37072fd.pth'
+resnet50InitParaPath = '../init/resnet50-11ad3fa6.pth'
+resnet101InitParaPath = '../init/resnet101-cd907fc2.pth'
+vgg11InitParaPath = '../init/vgg11-8a719046.pth'
+vgg13InitParaPath = '../init/vgg13-19584684.pth'
+vgg16InitParaPath = '../init/vgg16-397923af.pth'
+vgg19InitParaPath = '../init/vgg19-dcbb9e9d.pth'
+'''用于存储bit翻转率的结果'''
+outputFileRoot = './result/'
 
-outputFileRoot = './bitFlip/result/'
+'''for xor substitute testing'''
+bit_1 = torch.tensor(1, dtype=torch.int32)
+bit_2 = torch.tensor(3, dtype=torch.int32)
+bit_3 = torch.tensor(7, dtype=torch.int32)
+bit_4 = torch.tensor(15, dtype=torch.int32)
+bit_5 = torch.tensor(31, dtype=torch.int32)
+bit_6 = torch.tensor(63, dtype=torch.int32)
+bit_7 = torch.tensor(127, dtype=torch.int32)
+bit_8 = torch.tensor(255, dtype=torch.int32)
+bit_9 = torch.tensor(511, dtype=torch.int32)
+bit_10 = torch.tensor(1023, dtype=torch.int32)
+bit_11 = torch.tensor(2047, dtype=torch.int32)
+bit_12 = torch.tensor(4095, dtype=torch.int32)
+bit_13 = torch.tensor(8191, dtype=torch.int32)
+bit_14 = torch.tensor(16383, dtype=torch.int32)
+bit_15 = torch.tensor(32767, dtype=torch.int32)
+bit_16 = torch.tensor(65535, dtype=torch.int32)
+bit_17 = torch.tensor(131071, dtype=torch.int32)
+bit_18 = torch.tensor(262143, dtype=torch.int32)
+bit_19 = torch.tensor(524287, dtype=torch.int32)
+bit_20 = torch.tensor(1048575, dtype=torch.int32)
+bit_21 = torch.tensor(2097151, dtype=torch.int32)
+bit_22 = torch.tensor(4194303, dtype=torch.int32)
+bit_23 = torch.tensor(8388607, dtype=torch.int32)
+bit_24 = torch.tensor(16777215, dtype=torch.int32)
+bit_25 = torch.tensor(33554431, dtype=torch.int32)
+bit_26 = torch.tensor(67108863, dtype=torch.int32)
+bit_27 = torch.tensor(134217727, dtype=torch.int32)
+bit_28 = torch.tensor(268435455, dtype=torch.int32)
+bit_29 = torch.tensor(536870911, dtype=torch.int32)
+bit_30 = torch.tensor(1073741823, dtype=torch.int32)
 
 
 def getBitFlipNum(str1, str2):
@@ -75,24 +108,13 @@ def showBitFlip(initParaPath, retrainParaPath, bitStartIdx, bitEndIdx, outputFil
     return
 
 
+def pthBitFLip(initParaPath, resnet50InitParaPath, )
+
 
 if __name__ == "__main__":
+    '''resnet50 bit flip'''
+    pthBitFLip()
     '''resnet50_2_CFIAR100'''
-    # showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-    #             0, 1, "./bitFlip/result/resnet502CFIAR100_low1Bit.csv" )
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 4, "./bitFlip/result/resnet502CFIAR100_low4Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 8, "./bitFlip/result/resnet502CFIAR100_low8Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 12, "./bitFlip/result/resnet502CFIAR100_low12Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 16, "./bitFlip/result/resnet502CFIAR100_low16Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 20, "./bitFlip/result/resnet502CFIAR100_low20Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                0, 23, "./bitFlip/result/resnet502CFIAR100_low23Bit.csv")
-    showBitFlip(resnet50InitParaPath, "./retrained_ImageNet2CIFAR100/res50_ep5.pth",
-                23, 26, "./bitFlip/result/resnet502CFIAR100_exp_low3Bit.csv")
+
 
 
