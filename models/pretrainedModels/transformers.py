1df34b6ea08a270acfdc1f610e4d2c51e8fa853a
MurmanskY <642015814@qq.com>
2024-09-11 13:54:10 +0800

    feat(model): add swin transformer model structure

--- models/pretrainedModels/transformers.py
+++ models/pretrainedModels/transformers.py
@@ -2,5 +2,7 @@
 for downloading pretrained parameters
 '''
 from torchvision.models import vit_b_16, ViT_B_16_Weights
+from torchvision.models import swin_b, Swin_B_Weights
 
 vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1)
+swin_b(weights=Swin_B_Weights.IMAGENET1K_V1)
